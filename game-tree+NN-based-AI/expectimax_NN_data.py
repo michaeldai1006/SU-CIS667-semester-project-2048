39c32edb29d410cff3fdb05ace16079d69bce158
michaeldai1006 <dtc74110@gmail.com>
2020-12-12 12:27:45 -0800

    expectimax_NN_data stub functions

---
+++ game-tree+NN-based-AI/expectimax_NN_data.py
@@ -0,0 +1,21 @@
+# TODO: #1
+# Encode a 2048_game.Game2048State, use onehot encoding
+# Will be called by get_batch
+# return a tensor
+def encode(state):
+    return
+    
+# TODO: #2
+# Generate training data for NN
+# Needs to call encode function to encode state
+# return: (input, output)
+# input: list of encoded states
+# outpit: list of correspond utilities
+def get_batch(board_size, num_games):
+    return
+
+# TODO: #3
+# Generate training data file
+# Call get_batch function, save training data as a ".pkl" file
+if __name__ == "__main__":
+    print("Not implemented yet")
