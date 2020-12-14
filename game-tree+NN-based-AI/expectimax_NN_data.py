4fc267602ef391df8366c79e6de048edfac02b7a
michaeldai1006 <dtc74110@gmail.com>
2020-12-14 01:10:06 -0800

    Finalize training data generation

--- game-tree+NN-based-AI/expectimax_NN_data.py
+++ game-tree+NN-based-AI/expectimax_NN_data.py
@@ -15,33 +15,35 @@ def encode(state):
 # Generate training data for NN
 # Needs to call encode function to encode state
 # return: (input, output)
-# input: list of encoded states
-# output: list of correspond utilities
+# input: stacked list of encoded states
+# output: stacked list of correspond utilities
 def get_batch(board_size, num_games):
     inputs = []
     outputs = []
 
-    # Start new game
-    state = Game2048State(board_size)
-    state = state.initialState()
+    # Perform 25 games
+    for _ in range(0, 25):
+        # Start new game
+        state = Game2048State(board_size)
+        state = state.initialState()
 
-    # Iterate num_games times
-    for i in range(0, num_games):
-        # Game ended
-        if state.isGameEnded()[0]: break
+        # Iterate num_games times
+        for i in range(0, num_games):
+            # Game ended
+            if state.isGameEnded()[0]: break
 
-        # Record state
-        inputs.append(encode(state))
+            # Record state
+            inputs.append(encode(state))
 
-        # Tree search next state
-        state = expect_ai.getNextState(state)
+            # Tree search next state
+            state = expect_ai.getNextState(state)
 
-        # Utility of tree searched state
-        node = expect_ai.Node(state, None)
-        outputs.append(tr.tensor([node.getUtility()]))
+            # Utility of tree searched state
+            node = expect_ai.Node(state, None)
+            outputs.append(tr.tensor([node.getUtility()]))
 
-        # Add new tile to the game
-        state = state.addNewTile()
+            # Add new tile to the game
+            state = state.addNewTile()
         
     # Stack tensor
     inputs = tr.stack(inputs)
@@ -54,5 +56,8 @@ def get_batch(board_size, num_games):
 # Generate training data file
 # Call get_batch function, save training data as a ".pkl" file
 if __name__ == "__main__":
-    board_size, num_games = 4, 100
-    with open("data%d.pkl" % board_size, "wb") as f: pk.dump(get_batch(board_size, num_games), f)
+    for board_size in [2,4,6,8,10]:
+        print("Working on data%d.pkl..." % board_size)
+        num_games = 50
+        with open("data%d.pkl" % board_size, "wb") as f: pk.dump(get_batch(board_size, num_games), f)
+        print("data%d.pkl generated" % board_size)
