aef7c61a446b0f0458ffe9c1d8fccf6d6f806012
michaeldai1006 <dtc74110@gmail.com>
2020-12-14 01:54:32 -0800

    Training data logic

--- game-tree+NN-based-AI/expectimax_NN_ai.py
+++ game-tree+NN-based-AI/expectimax_NN_ai.py
@@ -6,7 +6,9 @@ import random
 import math
 import numpy as np
 import torch as tr
-from torch.nn import Sequential, Conv2d, Linear, Flatten, LeakyReLU, Tanh
+import pickle as pk
+import matplotlib.pyplot as pt
+from torch.nn import Sequential, Linear, Flatten
 
 # Max searching depth
 search_max_depth = 0
@@ -14,35 +16,14 @@ search_max_depth = 0
 # Processed tree nodes
 processed_nodes = 0
 
-### HW4 COPY
-def BlockusNet1(board_size):
-    net = Sequential(
-        Flatten(),
-        Linear(in_features=board_size*board_size,out_features=1)
-    )
-    return net
-
-def calculate_loss(net, x, y_targ):
-    y = net(x)
-    loss = tr.sum((y - y_targ)**2)
-    return y, loss
-
-def optimization_step(optimizer, net, x, y_targ):
-    optimizer.zero_grad()
-    y, loss = calculate_loss(net, x , y_targ)
-    loss.backward()
-    optimizer.step()
-    return y, loss
-
-####
-
 # #4
 # Generate NN module
 # return type: torch.nn.Module
 def getNet(board_size):
     return Sequential(
         Flatten(),
-        Linear(in_features=board_size*board_size,out_features=1)
+        Linear(in_features=board_size*board_size,out_features=board_size),
+        Linear(in_features=board_size,out_features=1)
     )
 
 # #5
@@ -189,42 +170,43 @@ def findExp(node, depth):
     return expected_utility
 
 if __name__ == "__main__":
-#### HW4
-    board_size = 4
-    net = BlockusNet1(board_size=board_size)
+    for board_size in [2,4,6,8,10]:
+        # NN instance
+        net = getNet(board_size)
 
-    import pickle as pk
-    with open("data%d.pkl" % board_size,"rb") as f: (x, y_targ) = pk.load(f)
+        # Load training data
+        with open("data%d.pkl" % board_size,"rb") as f: (x, y_targ) = pk.load(f)
 
-    # Optimization loop
-    optimizer = tr.optim.Adam(net.parameters())
-    train_loss, test_loss = [], []
-    shuffle = np.random.permutation(range(len(x)))
-    split = 10
-    train, test = shuffle[:-split], shuffle[-split:]
+        # Prepare training data
+        train_loss = []
+        shuffle = np.random.permutation(range(len(x)))
+        train = shuffle[:-10]
 
-    print(x[train])
+        # Gradient descent
+        optimizer = tr.optim.Adam(net.parameters())
+        for epoch in range(1000):
+            optimizer.zero_grad()
+            y_train = net(x[train])
+            e_train = tr.sum((y_train - y_targ[train])**2)
+            e_train.backward()
+            optimizer.step()
+            train_loss.append(e_train.item() / (len(shuffle)-10))
 
-    for epoch in range(5000):
-        y_train, e_train = optimization_step(optimizer, net, x[train], y_targ[train])
-        y_test, e_test = calculate_loss(net, x[test], y_targ[test])
-        if epoch % 10 == 0: print("%d: %f (%f)" % (epoch, e_train.item(), e_test.item()))
-        train_loss.append(e_train.item() / (len(shuffle)-split))
-        test_loss.append(e_test.item() / split)
-    
-    tr.save(net.state_dict(), "model%d.pth" % board_size)
-    
-    import matplotlib.pyplot as pt
-    pt.plot(train_loss,'b-')
-    pt.plot(test_loss,'r-')
-    pt.legend(["Train","Test"])
-    pt.xlabel("Iteration")
-    pt.ylabel("Average Loss")
-    pt.show()
-    
-    pt.plot(y_train.detach().numpy(), y_targ[train].detach().numpy(),'bo')
-    pt.plot(y_test.detach().numpy(), y_targ[test].detach().numpy(),'ro')
-    pt.legend(["Train","Test"])
-    pt.xlabel("Actual output")
-    pt.ylabel("Target output")
-    pt.show()
+        # Save trained data
+        tr.save(net.state_dict(), "model%d.pth" % board_size)
+        
+        # Plot iteration vs. average loss
+        pt.plot(train_loss,'b-')
+        pt.legend(["Train"])
+        pt.xlabel("Iteration" )
+        pt.ylabel("Average Loss")
+        pt.title("Iteration vs. Average Loss - %dx%d game size" % (board_size, board_size))
+        pt.show()
+        
+        # Plot actual output vs. target loss
+        pt.plot(y_train.detach().numpy(), y_targ[train].detach().numpy(),'bo')
+        pt.legend(["Train"])
+        pt.xlabel("Actual output")
+        pt.ylabel("Target output")
+        pt.title("Actual output vs. Target output - %dx%d game size" % (board_size, board_size))
+        pt.show()
